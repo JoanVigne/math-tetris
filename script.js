b83d03ef5797229d016b4cc7ff682c6d4eb03d34
Joan <joan.vigne@hotmail.fr>
2024-06-01 12:06:25 +0100

    remove in the table and in the grid, and score

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const container = document.querySelector(".container");
 const resultsContainer = document.querySelector(".results");
 // size of the game : 15 - 10
 const grid = Array.from({ length: 15 }, () => Array(10).fill(false));
-
+const scoreContainer = document.querySelector(".score");
+let score = 0;
 function createACube() {
   const cube = document.createElement("div");
   cube.innerText = Math.floor(Math.random() * 11);
@@ -36,25 +37,48 @@ function placeItem(y, x) {
   // NEW LINE
   if (grid[y - 1].every((cell) => cell !== false)) {
     let count = grid[y - 1].reduce((a, b) => Number(a) + Number(b), 0);
+    let resultOfThisLine = document.querySelector(".resultOfLine" + y);
+    if (resultOfThisLine) {
+      // if line already have a result, we remove it and replace it
+      resultOfThisLine.remove();
+    }
     const resultline = document.createElement("div");
+    resultline.classList.add("resultOfLine" + y);
     resultline.innerText = count;
     resultline.classList.add("result");
     resultline.style.gridRowStart = y;
     resultsContainer.appendChild(resultline);
     //! IF ROW IS 50
-    if (count === 50) {
+    /* if (count === 50) { */
+    if (count >= 20) {
+      console.log("row is equal to 50");
+      score = score + count;
+      scoreContainer.innerText = score;
+
       /*       for (let i = 0; i < grid.length; i++) {
         grid[i] = grid[i].map(() => false);
       } */
-      for (let i = y; i < grid.length; i++) {
+      /* for (let i = 0; i < grid[y - 1].length; i++) {
+        if (i - 1 !== false) {
+          console.log("false");
+          // grid[i] = [...grid[i - 1]]; // error :  TypeError: grid[(i - 1)] is not iterable
+        } else {
+          console.log("else");
+          grid[i] = grid[i].map(() => false);
+        }
+      } */
+      /* for (let i = y; i < grid.length; i++) {
         if (i - 1 !== false) {
           grid[i] = [...grid[i - 1]];
         } else {
           grid[i] = grid[i].map(() => false);
         }
-      }
+      } */
       // to remove the line
-      /* for (let i = 0; i < grid[y - 1].length; i++) {
+      for (let i = 0; i < grid[y - 1].length; i++) {
+        // clear the grid table
+        grid[y - 1][i] = false;
+        // remove the cell from the DOM
         const cell = document.querySelector(
           `.occupied[style*="grid-column-start: ${
             i + 1
@@ -63,16 +87,28 @@ function placeItem(y, x) {
         if (cell) {
           cell.remove();
         }
-      } */
-      // Shift all rows above the deleted row down by one
+      }
     }
     //! IF ROW IS 50 end
   }
-
+  // check if row inder is empty
+  checkUnderRow();
+  // new cube to keep playing
   createACube();
 }
 createACube();
 
+function checkUnderRow() {
+  // check if row under is empty starting from the last one
+  console.log("grid : ", grid);
+  for (let i = grid.length - 1; i > 0; i--) {
+    if (grid[i].every((cell) => cell === false)) {
+      console.log("row under is empty", i);
+    } else {
+      console.log("row under is not empty");
+    }
+  }
+}
 function lost() {
   alert("You lost the game");
 }
